f201f2c004296758b6093c859d80e5fc82237bc6
tamzid620 <tamzid620@gmail.com>
2023-03-22 13:02:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+
+
+
+// night mode ----------------------------
+
+document.getElementById('night-mode').addEventListener('click', function(){
+
+    document.body.style.backgroundImage ='linear-gradient(-90deg,#000000 ,#923CB5)';
+
+    document.getElementById('myRow').style.backgroundImage='linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+
+    document.getElementById('div').style.backgroundImage='linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+
+    document.getElementById('div1').style.backgroundImage='linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+
+    document.getElementById('div2').style.backgroundImage='linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+
+    document.getElementById('div3').style.backgroundImage='linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+
+    document.getElementById('div4').style.backgroundImage='linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+
+    document.getElementById('div5').style.backgroundImage='linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+
+    document.getElementById('div6').style.backgroundImage='linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+
+    document.getElementById('button-readMore').style.backgroundImage = 'linear-gradient(-90deg,#f8f8f8a4 , #615f5f)';
+
+    document.getElementById('footer').style.backgroundImage = 'linear-gradient(-90deg,#000000 ,#04619F)';
+})
+
+// light mode ------------------------------
+
+document.getElementById('light-mode').addEventListener('click', function(){
+
+    document.body.style.backgroundImage =' linear-gradient(-90deg, #662D8C , #ED1E79 )';
+
+    document.getElementById('myRow').style.backgroundImage='linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+
+    document.getElementById('div').style.backgroundImage='linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+
+    document.getElementById('div1').style.backgroundImage='linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+
+    document.getElementById('div2').style.backgroundImage='linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+
+    document.getElementById('div3').style.backgroundImage='linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+
+    document.getElementById('div4').style.backgroundImage='linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+
+    document.getElementById('div5').style.backgroundImage='linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+
+    document.getElementById('div6').style.backgroundImage='linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+
+    document.getElementById('button-readMore').style.backgroundImage = 'linear-gradient(-90deg,#a9f1dfa4 , #FFBBBB)';
+    
+    document.getElementById('footer').style.backgroundImage = 'linear-gradient(-90deg,  #d7cfcf ,#f8f8f8a4 )';
+})
